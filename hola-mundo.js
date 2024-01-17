c1f21c1a51b152e6a911adef5d60b04516d5dda3
Johan.Bohorquez <johan.bohorquez@perficient.com>
2024-01-17 10:44:21 -0500

    add docker compose file and new changes in hola-mundo.js

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,4 +1,26 @@
 // hola-mundo.js
-console.log('¡Hola Mundo en Javascript!');
+//console.log('¡Hola Mundo en Javascript!');
  
-alert("You connected to the following path: johanbohorquez2021/cron-ticker_ramp-up_devops:2.0.7")
+// alert("You connected to the following path: johanbohorquez2021/cron-ticker_ramp-up_devops:2.0.7")
+
+const http = require('http');
+const MongoClient = require('mongodb').MongoClient;
+
+const server = http.createServer(async (req, res) => {
+  // Conéctate a la base de datos MongoDB
+  const client = await MongoClient.connect('mongodb://mongo-db:27017', { useNewUrlParser: true });
+  const db = client.db('mydatabase');
+
+  // Lógica de la aplicación utilizando la base de datos
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('¡Hola Mundo!\n');
+
+  // Cierra la conexión a la base de datos
+  client.close();
+});
+
+const PORT = 80;
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
